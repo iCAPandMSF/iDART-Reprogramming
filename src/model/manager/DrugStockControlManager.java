2230cdaf4f8d0ac316221ccaa7210e3d88e77f52
marceldfs <mdfsaraiva@gmail.com>
2015-05-20 14:37:00 +0200

    Correccao do AMC e adicao dos reports

--- src/model/manager/DrugStockControlManager.java
+++ src/model/manager/DrugStockControlManager.java
@@ -164,15 +164,15 @@ public class DrugStockControlManager {
 
 		Date endDateFinal = cal.getTime();
 		
-		Long qty = ((Long) session
+		Double qty = ((Double) session
 				.createQuery(
-						"select sum(pd.amount)/sum(pd.stock.drug.packSize) "
+						"select sum(pd.amount)/avg(pd.stock.drug.packSize) "
 						+ "from PackagedDrugs pd where pd.stock.drug = :drug "
 						+ "and pd.parentPackage.packDate between :startDate and :endDate "
 						+ "and pd.parentPackage.prescription is not null ")
 						.setInteger("drug", drugId).setDate("startDate", startDate).setDate(
 										"endDate", endDate).uniqueResult());
-		return qty == null ? new Long(0) : qty;
+		return qty == null ? new Long(0) : qty.longValue();
 	}
 	
 	private static Long getExistingStock(Session session,int drugId)
